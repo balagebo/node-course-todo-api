968735340fb27e3fe7372d5bdcfa9b76be15dafd
Bóta Balázs (PW) <bota.balazs@pwstudio.hu>
2017-06-19 21:02:15 +0200

    Delete script

---
+++ playground/delete.js
@@ -0,0 +1,12 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    db.collection('Users').deleteMany({_id: '123'}).then((res) => {
+        console.log(res.result.n);
+    });
+    // db.close();
+});
